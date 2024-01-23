5e585ea472a078f62c099b6e8702942f03082741
Sam <snmriley@gmail.com>
2024-01-23 22:33:50 +0800

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-from tkinter import Tk
-from tkinter import filedialog as fd
-from tkinter.filedialog import askopenfilename
-
-Tk().withdraw()
-#filename = askopenfilename(filetypes=[("PDF document",".pdf")]) 
-filename = fd.askopenfile()
-print(filename)
